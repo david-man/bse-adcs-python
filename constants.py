44905a12e468619c38ff53c6e608340c1248b8f8
david-man <86388290+david-man@users.noreply.github.com>
2025-02-14 08:49:59 -0500

    addition of quaternion mekf predict

--- constants.py
+++ constants.py
@@ -2,14 +2,14 @@
 
 import numpy as np
 
-DT = 10
+DT = 5
 MAG_BIAS = 10
 PHOTO_BIAS = 0
 
-ORBIT_Q = np.eye(6)*0.001#change
+ORBIT_Q = np.eye(6)#change
 ORBIT_R = np.array([MAG_BIAS**2])
 
-BDOT_Q = np.eye(9)#change
+BDOT_Q = np.eye(9) * 10#change
 BDOT_R = np.eye(3)*(MAG_BIAS**2)
 
 ROT_Q = np.eye(6) * 10#use the Q equation from Table II of the "backup satellite" paper
